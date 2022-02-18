c60740612053a1729fdc31576f92e3b7d2c81121
Acer <Acer@DESKTOP-F9ONNJE>
2022-02-18 19:40:50 +0530

    abstract

---
+++ Abstractprog/src/Abstractprogramsss/AAbstractprograms.java
@@ -0,0 +1,29 @@
+package Abstractprogramsss;
+
+public class AAbstractprograms {
+	
+	public class AccessModifier{
+		int nos;
+		float hrs;
+		int noc;
+		void pupils() {//m1
+			System.out.println("Number of Students");
+		}
+		void displayHours() {//m2
+			System.out.println("Number of Hours");
+		}
+		void displayClass() {//m3
+			System.out.println("Number of Class");
+		}
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			AccessModifier am = new AccessModifier();
+			am.displayStudents();
+		}
+
+	}
+
+
+}
---
+++ Abstractprog/src/Abstractprogramsss/Abstractprograms.java
@@ -0,0 +1,5 @@
+package Abstractprogramsss;
+
+public class Abstractprograms {
+
+}
